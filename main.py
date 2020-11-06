6904fc29b49a08c0074e3f8c0cc4944b0434c39e
khoidd <khoidd@vng.com.vn>
2020-11-06 16:44:21 +0700

    update callbacks.py

--- main.py
+++ main.py
@@ -9,8 +9,16 @@ if __name__ == "__main__":
     generator_train_data = dataset.VoiceNpyDataGenerator("dataset/npy")
 
     logger.info("Model is starting ...")
-    model_v1 = model.gen_model_v1()
-    cp_callback = callbacks.get_check_point_callback()
 
-    model_v1.fit(generator_train_data, epochs=32, callbacks=[cp_callback])
+    model_v1 = model.gen_model_v1()
+
+    cp_callback = callbacks.get_check_point_callback()
+    cp_tensorboard = callbacks.get_tensor_board_callback()
+
+    model_v1.fit(
+        generator_train_data,
+        epochs=64,
+        callbacks=[cp_callback, cp_tensorboard],
+    )
+
     logger.info("Model is done!")
--- src/callbacks.py
+++ src/callbacks.py
@@ -2,8 +2,17 @@ import os
 import tensorflow.keras.callbacks as C
 
 
-def get_check_point_callback(checkpoint_path="checkpoint"):
-    os.makedirs(checkpoint_path, exist_ok=True)
-    cp_callback = C.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True)
+def get_check_point_callback(checkpoint_path="model/cp-{epoch:04d}.ckpt"):
+    cp_callback = C.ModelCheckpoint(
+        filepath=checkpoint_path,
+        save_weights_only=True,
+        period=5,
+    )
 
     return cp_callback
+
+
+def get_tensor_board_callback(log_path="tensor-board"):
+    cp_tensorboard = C.TensorBoard(log_path, histogram_freq=1)
+
+    return cp_tensorboard
